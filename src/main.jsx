8cbf37a1f673f776c26e5817c1dd64c271a33451
sumitpathak914 <sumitpathakofficial914@gmail.com>
2026-08-21 20:50:54 +0530

    fewfw

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,13 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AuthProvider } from './context/AuthContext'; // Make sure this path is correct
+import './index.css';
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 );
