e41c5fa56073eec334e0d9bd7fac508552781276
sumitpathak914 <sumitpathakofficial914@gmail.com>
2026-08-21 20:59:10 +0530

    sdfsfsf

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,22 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// );
+
+
+import React from "react";
+import {createRoot} from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
-
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <AuthProvider>
     <App />
-  </StrictMode>,
+  </AuthProvider>
 );
-
-
-
