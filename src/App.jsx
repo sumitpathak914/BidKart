3994ac77049584b57deab39bba07a7c69b67bbd3
sumitpathak914 <sumitpathakofficial914@gmail.com>
2026-08-21 21:18:22 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useEffect } from "react";
-import { BrowserRouter, Route, Routes, useLocation, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  useLocation
+} from "react-router-dom";
 import AllAuctionsPage from "./component/AllAuctionsPage";
 import AuctionDetailsPage from "./component/AuctionDetailsPage";
+import LoginPage from "./component/AuthModal";
 import BiddingDashboardPage from "./component/BiddingDashboardPage";
 import BiddingDetailsPage from "./component/BiddingDetailsPage";
 import BottomNav from "./component/BottomNav";
 import CommunityChatPage from "./component/CommunityChatPage";
 import CommunityPage from "./component/CommunityPage";
 import ExplorePage from "./component/Explorepage";
+import HelpAndSupportPage from "./component/HelpAndSupportPage";
 import HomePage from "./component/Homepage";
+import KycPage from "./component/KycPage";
 import MyCommunityPage from "./component/MyCommunityPage";
 import MyCustomersPage from "./component/MyCustomersPage";
 import MyListingDetailsPage from "./component/MyListingDetailsPage";
@@ -17,16 +25,8 @@ import MyStockPage from "./component/MyStockPage";
 import ProfilePage from "./component/ProfilePage";
 import QRScannerPage from "./component/QRScannerPage";
 import SellPage from "./component/SellPage";
-import ShopDetailsPage from "./component/ShopDetailsPage";
-import KycPage from "./component/KycPage";
 import SettingsPage from "./component/SettingsPage";
-import HelpAndSupportPage from "./component/HelpAndSupportPage";
-import LoginPage from "./component/AuthModal";
-
-
-
-
-
+import ShopDetailsPage from "./component/ShopDetailsPage";
 
 function LayoutWithBottomNav() {
   const location = useLocation();
@@ -38,7 +38,7 @@ function LayoutWithBottomNav() {
 
   // List of paths where you DO NOT want the Bottom Navigation to show
   const hideBottomNavPaths = [
-    "/login",
+    "/",
     "/scan-qr",
     "/shop",
     "/auction",
@@ -52,7 +52,7 @@ function LayoutWithBottomNav() {
 
   // Check if the current pathname starts with any of the paths in the list
   const shouldShowBottomNav = !hideBottomNavPaths.some((path) =>
-    location.pathname.startsWith(path)
+    location.pathname.startsWith(path),
   );
 
   // Don't show BottomNav on login page
@@ -62,111 +62,40 @@ function LayoutWithBottomNav() {
     <div className="min-h-screen bg-[#F6F5F1] pb-24">
       <Routes>
         {/* Public Routes */}
-        <Route path="/login" element={<LoginPage />} />
-        
+        <Route path="/" element={<LoginPage />} />
+
         {/* Protected Routes */}
-        <Route path="/home" element={
-         
-            <HomePage />
-         
-        } />
-        <Route path="/explore" element={
-         
-            <ExplorePage />
-         
-        } />
-        <Route path="/sell" element={
-         
-            <SellPage />
-         
-        } />
-        <Route path="/community" element={
-         
-            <CommunityPage />
-         
-        } />
-        <Route path="/profile" element={
-          
-            <ProfilePage />
-         
-        } />
-        <Route path="/scan-qr" element={
-          
-            <QRScannerPage />
-          
-        } />
-        <Route path="/shop/:shopId" element={
-         
-            <ShopDetailsPage />
-         
-        } />
-        <Route path="/auction/:auctionId" element={
-          
-            <AuctionDetailsPage />
-         
-        } />
-        <Route path="/all-auctions" element={
-         
-            <AllAuctionsPage />
-          
-        } />
-        <Route path="/community-chat/:communityId" element={
-         
-            <CommunityChatPage />
-          
-        } />
-        <Route path="/settings" element={
-         
-            <SettingsPage />
-         
-        } />
-        <Route path="/my-listings" element={
-         
-            <MyListingsPage />
-         
-        } />
-        <Route path="/help" element={
-        
-            <HelpAndSupportPage />
-          
-        } />
-        <Route path="/my-listing-details/:listingId" element={
-          
-            <MyListingDetailsPage />
-         
-        } />
-        <Route path="/kyc" element={
-          
-            <KycPage />
-          
-        } />
-        <Route path="/my-community" element={
-        
-            <MyCommunityPage />
-          
-        } />
-        <Route path="/my-customers" element={
-         
-            <MyCustomersPage />
-          
-        } />
-        <Route path="/my-stock" element={
-          
-            <MyStockPage />
-         
-        } />
-        <Route path="/bidding-dashboard" element={
-         
-            <BiddingDashboardPage />
-         
-        } />
-        <Route path="/bidding-details/:auctionId" element={
-          
-            <BiddingDetailsPage />
-         
-        } />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/explore" element={<ExplorePage />} />
+        <Route path="/sell" element={<SellPage />} />
+        <Route path="/community" element={<CommunityPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/scan-qr" element={<QRScannerPage />} />
+        <Route path="/shop/:shopId" element={<ShopDetailsPage />} />
+        <Route path="/auction/:auctionId" element={<AuctionDetailsPage />} />
+        <Route path="/all-auctions" element={<AllAuctionsPage />} />
+        <Route
+          path="/community-chat/:communityId"
+          element={<CommunityChatPage />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/my-listings" element={<MyListingsPage />} />
+        <Route path="/help" element={<HelpAndSupportPage />} />
+        <Route
+          path="/my-listing-details/:listingId"
+          element={<MyListingDetailsPage />}
+        />
+        <Route path="/kyc" element={<KycPage />} />
+        <Route path="/my-community" element={<MyCommunityPage />} />
+        <Route path="/my-customers" element={<MyCustomersPage />} />
+        <Route path="/my-stock" element={<MyStockPage />} />
+        <Route path="/bidding-dashboard" element={<BiddingDashboardPage />} />
+        <Route
+          path="/bidding-details/:auctionId"
+          element={<BiddingDetailsPage />}
+        />
       </Routes>
-      {!isLoginPage && shouldShowBottomNav  && <BottomNav />}
+      {!isLoginPage && shouldShowBottomNav && <BottomNav />}
     </div>
   );
 }
@@ -174,9 +103,7 @@ function LayoutWithBottomNav() {
 export default function App() {
   return (
     <BrowserRouter>
-     
-        <LayoutWithBottomNav />
-     
+      <LayoutWithBottomNav />
     </BrowserRouter>
   );
 }
