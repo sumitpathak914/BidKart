6529f25e54a76c5e996b731fb667ab1ca50b76c7
sumitpathak914 <sumitpathakofficial914@gmail.com>
2026-08-21 21:02:28 +0530

    testing cehck

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import App from "./App.jsx";
-// import "./index.css";
-
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// );
-
-
-import React from "react";
-import {createRoot} from "react-dom/client";
-import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
+
 createRoot(document.getElementById("root")).render(
-  <AuthProvider>
+  <StrictMode>
     <App />
-  </AuthProvider>
+  </StrictMode>,
 );
+
+
+// import React from "react";
+// import {createRoot} from "react-dom/client";
+// import App from "./App";
+// import { AuthProvider } from "./context/AuthContext";
+// import "./index.css";
+// createRoot(document.getElementById("root")).render(
+//   <AuthProvider>
+//     <App />
+//   </AuthProvider>
+// );
